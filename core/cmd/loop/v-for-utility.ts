b32edfb718bd2b167a2b84c13a51f7e9841c8a6d
mutian <mutian-xiangcai@outlook.com>
2024-05-18 22:02:52 +0800

    version 1.3.56

--- core/cmd/loop/v-for-utility.ts
+++ core/cmd/loop/v-for-utility.ts
@@ -1,4 +1,5 @@
 import {extractForMap} from "./v-map";
+import {resolver_map_single} from "./v-map-utility";
 
 /**
  * 遍历根元素
@@ -70,29 +71,11 @@ export function resolver_array_multi(elements:HTMLCollection, index:number, data
         }
 
         //渲染文档
-        if (elements[i].hasAttribute("for-document")){
+        if (elements[i].hasAttribute("@for-document")){
 
             elements[i].removeAttribute("@for-document");
 
-            if (data instanceof Array){
-
-                data.forEach(function (value: any) {
-
-                    //从数组中取出一条信息，然后开始渲染
-                    extractForMap(elements[i].parentNode, elements[i], value);
-
-                    i++;
-                })
-
-                //将模板节点删除
-                elements[i].parentNode.removeChild(elements[i]);
-
-                //重新回到原来的索引值
-                i--;
-            }else {
-
-                console.log("Instruction @list need an array datatype to extract!");
-            }
+            resolver_map_single(elements[i],data);
         }
 
         //渲染信息
